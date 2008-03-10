04108496d454eb843978fe6aaa515686043579e0
LaMont Jones <lamont@debian.org>
2008-03-10 11:38:41 -0600

    add "order random_1" support (return one random RR)
    Clients who implement RFC 3484, Section 6, Rule 9 for IPv4 addresses
    create serious issues for domains that use rrset-order cyclic or random to do
    load balancing, since they erroneously decide that the host with the longest
    prefix in common with their own address is "closest", while no such relationship
    exists in IPv4.
    
    While technically incorrect, returning exactly one A RR from a random ordering
    works around this behavior.  Using a short TTL is a good idea, for fault
    tollerance.
    
    Signed-off-by: LaMont Jones <lamont@ubuntu.com>

--- lib/dns/include/dns/rdataset.h
+++ lib/dns/include/dns/rdataset.h
@@ -206,6 +206,8 @@ struct dns_rdataset {
 #define DNS_RDATASETATTR_OPTOUT		0x00100000	/*%< OPTOUT proof */
 #define DNS_RDATASETATTR_NEGATIVE	0x00200000
 
+#define DNS_RDATASETATTR_SINGLE		0x10000000	/* Only return 1 answer */
+
 /*%
  * _OMITDNSSEC:
  * 	Omit DNSSEC records when rendering ncache records.
